c71cf9e63a003ac0b99900c7bf2a236d6518a900
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2019-02-16 10:37:01 -0200

    carregar imagens da pagina direto do github

--- index2.php
+++ index2.php
@@ -1,4 +1,4 @@
 <?php
-$home = file_get_contents('https://raw.githubusercontent.com/pentahobrasil/pentahoday2019/master/index.html');
+$home = file_get_contents('https://raw.githubusercontent.com/pentahobrasil/pentahoday2019/master/index-git.html');
 echo $home;
 ?>
