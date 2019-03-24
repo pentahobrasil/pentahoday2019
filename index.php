fc4c321c32abf93afde8acf0afb0c5d232625009
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2019-03-24 19:11:25 -0300

    Alteracoes da semana

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-$home = file_get_contents('http://www.ambientelivre.com.br/saas/evento/sitepentahoday2019/index-git.html');
+
+$host = $_SERVER['HTTP_HOST'];
+if ( $host == localhost ) 
+   $home = file_get_contents('index-git.html'); 
+else
+   $home = file_get_contents('http://www.ambientelivre.com.br/saas/evento/sitepentahoday2019/index-git.html');
 echo $home;
 ?>
